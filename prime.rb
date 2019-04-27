a81438d0eb901084d3c74972cad8e4fa1ecf5d6e
Tanuka Das <das.tanuka@yahoo.com>
2019-04-27 03:39:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,16 @@
 # Add  code here!
 
 def prime?(int)
-  if int <= 1
+
+end
+
+
+
+=begin  if int <= 1
     return false
   elsif int == 2
     return true
   else
     (2..int/2).none? { |i| int % i == 0}
   end
-end
+=end
