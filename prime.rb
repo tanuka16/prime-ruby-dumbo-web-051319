e636c2408681d803caba80a120ef68c2ab000a1d
Tanuka Das <das.tanuka@yahoo.com>
2019-04-27 03:48:52 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
 # Add  code here!
+
+def prime?(int)
+  # check if num is less than 2.
+  if int <= 1
+      return false
+    elsif int == 2
+      return true
+    else
+      (2..int/2).none? { |i| int % i == 0}
+    end                
+end
+
+
+
+=begin  if int <= 1
+    return false
+  elsif int == 2
+    return true
+  else
+    (2..int/2).none? { |i| int % i == 0}
+  end
+=end
